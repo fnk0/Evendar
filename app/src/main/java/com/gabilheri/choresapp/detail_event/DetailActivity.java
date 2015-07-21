6d19b12960db56932002c84442fd4b3f9db22135
marcus <marcusandreog@gmail.com>
2015-07-21 11:55:06 -0500

    * Switched MapFragment to MapView * MapView uses LiteMode removing the need of a heavy OpenGL context for a small map * Fixed the parallax behavior of the Google Map.

--- app/src/main/java/com/gabilheri/choresapp/detail_event/DetailActivity.java
+++ app/src/main/java/com/gabilheri/choresapp/detail_event/DetailActivity.java
@@ -13,8 +13,9 @@ import com.gabilheri.choresapp.R;
 import com.gabilheri.choresapp.adapters.MyFragmentAdapter;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -40,12 +41,17 @@ public class DetailActivity extends BaseActivity implements OnMapReadyCallback {
     @Bind(R.id.viewpager)
     ViewPager viewPager;
 
+    @Bind(R.id.mapview)
+    MapView mapView;
+
+    GoogleMap map;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
         ViewCompat.setElevation(collapsingToolbarLayout, 20);
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             if (toolbar != null) {
@@ -54,22 +60,47 @@ public class DetailActivity extends BaseActivity implements OnMapReadyCallback {
             collapsingToolbarLayout.setElevation(0f);
         }
 
+        mapView.onCreate(savedInstanceState);
+        MapsInitializer.initialize(this);
+        mapView.getMapAsync(this);
+
         enableBackNav();
         if(viewPager != null) {
             MyFragmentAdapter adapter = new MyFragmentAdapter(getFragmentManager());
             adapter.addFragment(new DetailFragment(), "Details");
             adapter.addFragment(new DetailFragment(), "Comments");
             viewPager.setAdapter(adapter);
-            tabLayout.setupWithViewPager(viewPager);
+            if (tabLayout != null) {
+                tabLayout.setupWithViewPager(viewPager);
+            }
         }
     }
 
     @Override
-    public void onMapReady(GoogleMap map) {
+    public void onMapReady(GoogleMap googleMap) {
+        this.map = googleMap;
         // Add a marker in Sydney, Australia, and move the camera.
         LatLng sydney = new LatLng(30.2702060, -97.6783);
         map.addMarker(new MarkerOptions().position(sydney).title("Some Location"));
         map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 
     @Override
