3e2ddb168b96d24172b60711efe653730ac2b083
marcus <marcusandreog@gmail.com>
2015-07-22 01:04:56 -0500

    * Improved mock data

--- app/src/main/java/com/gabilheri/choresapp/MockUtils.java
+++ app/src/main/java/com/gabilheri/choresapp/MockUtils.java
@@ -27,56 +27,62 @@ public class MockUtils {
             R.drawable.ic_twitter
     };
 
-    public static String[] userPics = {
+    public static String[] maleUserPics = {
             "https://lh6.googleusercontent.com/-PLz-1wFMskM/U1VdxVwcXgI/AAAAAAAAMvw/JUU7tw4CWms/w1674-h1676-no/1397661337286.jpg",
-            "https://33.media.tumblr.com/avatar_709fd8f3d9a7_128.png",
             "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSIAbiG7YuVqH5RJVgabMrUlIyB0mJyryz4lYjnwq71LgBc-7Rx",
-            "https://38.media.tumblr.com/avatar_ea958e421800_128.png",
-            "https://lh5.googleusercontent.com/-uYfmMfHEgc0/U8tmAmxQBfI/AAAAAAAAHFU/Lu1sr2s2B_o/s1676-no/6c1565d7-996f-448f-b360-d196f9eaa2d4",
             "https://lh6.googleusercontent.com/--Vbi85jnmwM/VBS56X-uA2I/AAAAAAAAOzw/czqKKZYqS8o/s1350-no/3e093ebb-fba4-4f46-9a23-8597083097b3",
             "https://lh4.googleusercontent.com/-d8fZAy-H32c/UewsNyNUdVI/AAAAAAAAAC4/-4jEoDjKQAI/w962-h960-no/2012-07-01_00.11.19.png",
             "https://lh3.googleusercontent.com/-yknrEtu-eVo/UiUpqd_jZgI/AAAAAAAAAms/tRsd4-bxVhQ/s512-no/e810c46f-9eb0-4c7c-bcab-06ac0c441e37",
-            "https://lh4.googleusercontent.com/-B6ITzWABbTc/Us5PiY921eI/AAAAAAAAABE/BjVd4_fy1qI/s812-no/2011-04-26%2B11.56.47.jpg",
-            "https://lh5.googleusercontent.com/-2tYvUEKmXuU/ThPhwkDIztI/AAAAAAAAIso/Y3L4JPGCQAw/w1680-h1676-no/IMG_1535.JPG",
-            "https://lh5.googleusercontent.com/-bSMw755G6SQ/VXFAvlx2A2I/AAAAAAABCb8/_MXmZuPG2wI/s1676-no/dots_2048.png",
             "https://lh4.googleusercontent.com/-ozt4HO8tTBA/U0B-GlkPqkI/AAAAAAAAJZQ/EEfuKWMxUdU/w1678-h1676-no/IMG_20140222_200540.jpg",
-            "https://lh4.googleusercontent.com/-r_Jj4RJim1c/VHg07lzQDsI/AAAAAAACZCU/AzhBIO0s618/s346-no/profilepic.png",
             "https://lh4.googleusercontent.com/-scZZhDY21bg/VIXFq5XnI3I/AAAAAAAAAEA/bFJqtZRikIE/w610-h616-no/download.png",
             "https://lh5.googleusercontent.com/-EN25hmNd-YE/VTe_XfWM3vI/AAAAAAAAFwY/KDHC1PBlyWU/s800-no/babbq15_400x400_round.png"
     };
 
-    public static String[] phrases = new String[] {
-            "Marcus is going to Whole Foods @3:00 PM",
-            "John is going to Chipotle @2:20 PM",
-            "Martha is going to watch The Avengers @8:30 PM",
-            "Sarah is going to Huts @6:30 PM",
-            "Peter is going to Starbucks @5:00 PM",
-            "Carissa is going to Austin Java @11:00 AM",
-            "Carter is going to Burger King @1:00 PM,",
-            "Mike is going to Trader Joe's @4:30 PM"
+    public static String[] femaleUserPics = {
+            "https://33.media.tumblr.com/avatar_709fd8f3d9a7_128.png",
+            "https://38.media.tumblr.com/avatar_ea958e421800_128.png",
+            "https://lh5.googleusercontent.com/-uYfmMfHEgc0/U8tmAmxQBfI/AAAAAAAAHFU/Lu1sr2s2B_o/s1676-no/6c1565d7-996f-448f-b360-d196f9eaa2d4",
+            "https://lh6.googleusercontent.com/--Vbi85jnmwM/VBS56X-uA2I/AAAAAAAAOzw/czqKKZYqS8o/s1350-no/3e093ebb-fba4-4f46-9a23-8597083097b3",
+            "https://lh3.googleusercontent.com/-yknrEtu-eVo/UiUpqd_jZgI/AAAAAAAAAms/tRsd4-bxVhQ/s512-no/e810c46f-9eb0-4c7c-bcab-06ac0c441e37",
+            "https://lh4.googleusercontent.com/-B6ITzWABbTc/Us5PiY921eI/AAAAAAAAABE/BjVd4_fy1qI/s812-no/2011-04-26%2B11.56.47.jpg",
+            "https://lh4.googleusercontent.com/-r_Jj4RJim1c/VHg07lzQDsI/AAAAAAACZCU/AzhBIO0s618/s346-no/profilepic.png",
     };
 
-    public static String[] names = new String[] {
+    public static String[] phrases =  {
+            " is going to Whole Foods @3:00 PM",
+            " is going to Chipotle @2:20 PM",
+            " is going to watch The Avengers @8:30 PM",
+            " is going to Huts @6:30 PM",
+            " is going to Starbucks @5:00 PM",
+            " is going to Austin Java @11:00 AM",
+            " is going to Burger King @1:00 PM,",
+            " is going to Trader Joe's @4:30 PM"
+    };
+
+    public static String[] maleNames = {
             "Marcus Gabilheri",
-            "Carissa Gabilheri",
-            "Angelina Jolie",
-            "Sandra Bullock",
             "Brad Pitt",
             "John Doe",
-            "Alice Connors",
             "Jerry Chan",
             "Kieran Stolorz",
             "Bruce Willis",
             "Some Really Long Name",
-            "Amanda Carter",
             "Bon Jovi",
-            "Hali Deubler",
             "Kyle Riedemann",
             "Ryan Dawkins",
             "Aaron Weaver"
     };
 
-    public static String[] smallIpsumStrings = new String[] {
+    public static String[] femaleNames = {
+            "Carissa Gabilheri",
+            "Angelina Jolie",
+            "Sandra Bullock",
+            "Alice Connors",
+            "Amanda Carter",
+            "Hali Deubler"
+    };
+
+    public static String[] smallIpsumStrings = {
             "Duis vitae scelerisque lacus. Ut facilisis massa et tortor aliquam suscipit. Nunc et metus turpis. Etiam laoreet, ipsum vel ultricies mollis, odio sem pretium leo, ac tincidunt metus nunc porttitor diam.",
             "Praesent imperdiet placerat ex, non sollicitudin tortor sagittis et. Nullam porta lacus dui, quis egestas quam lacinia quis. Donec nunc risus, sagittis eu tellus vel, pharetra vehicula sem.",
             "Nullam a vestibulum diam. Proin tincidunt rutrum venenatis.",
@@ -103,7 +109,7 @@ public class MockUtils {
             "Suspendisse erat leo, luctus et vestibulum viverra, congue eget quam."
     };
 
-    public static String[] largeIpsumStrings = new String[] {
+    public static String[] largeIpsumStrings = {
             "Suspendisse dapibus enim facilisis lacus lacinia auctor. Quisque nulla ipsum, maximus nec ante eu, viverra euismod velit. Phasellus gravida nisi in molestie ultricies. Sed at ultrices est, vehicula lobortis purus. Quisque rutrum ex consectetur lobortis sagittis. Proin venenatis magna eget nibh eleifend rutrum. Vestibulum vitae tellus mi. Morbi semper iaculis lacus commodo viverra. Aliquam nec lacus volutpat tortor ultricies maximus id a est. Aliquam sem erat, faucibus vitae tortor eu, pulvinar consectetur ligula. Pellentesque mattis tempus tortor. Maecenas cursus scelerisque nisl, sit amet convallis sapien bibendum vitae. Cras maximus pellentesque nisi vel laoreet.",
             "Cras vestibulum ligula a magna lacinia porta. Cras cursus quam risus, id posuere lorem hendrerit ullamcorper. Aliquam at massa nisl. Integer mattis, sem ut ornare volutpat, felis nunc finibus est, a commodo ex lorem elementum sapien. Aliquam sed lobortis nisi. Nam luctus ipsum odio, at aliquam sem molestie ut. Curabitur ut commodo mi, sed accumsan nibh. Suspendisse venenatis felis quam, sit amet mattis turpis gravida a. Vestibulum consequat tortor libero, in hendrerit dui placerat vel. Nunc rhoncus justo a ante fringilla, in vehicula quam tempor. Nunc vehicula sapien quis ullamcorper efficitur. Suspendisse in orci gravida, ornare elit vel, suscipit mauris. Ut a varius mauris. Aenean et lectus vel justo posuere congue quis a quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis condimentum dui non magna fermentum, ut eleifend orci tempus.",
             "Cras aliquam ac est eu eleifend. Vivamus facilisis eu diam non pharetra. Pellentesque laoreet metus in ante bibendum, et cursus lacus faucibus. Duis vel pulvinar elit. Phasellus ultrices felis id mauris lobortis, a sollicitudin enim imperdiet. Duis ac laoreet mauris. Donec eleifend ipsum arcu, sit amet imperdiet metus rhoncus vel. Integer luctus diam quis vehicula sollicitudin. Aliquam nisi nisl, euismod ut nisl id, sodales rhoncus nisi. Nulla facilisi. Suspendisse sed nisi libero. Donec ut velit luctus, sollicitudin nisl convallis, sagittis eros. Fusce libero velit, accumsan sed imperdiet a, consequat sed diam. Aenean consequat felis et ligula sagittis, in blandit erat cursus. Aliquam quis consequat diam. Pellentesque commodo tortor vel neque iaculis, eu congue est sollicitudin.",
@@ -119,18 +125,20 @@ public class MockUtils {
 
     public static Comment getRandomComment() {
         Comment c = new Comment();
-        c.setUserPicture(userPics[random.nextInt(userPics.length)]);
+        int r = random.nextInt();
+        c.setUserPicture(getUserPicture(r));
+        c.setUserName(getUserName(r));
         c.setFavoritesCount(random.nextInt(40));
         c.setComment(smallIpsumStrings[random.nextInt(smallIpsumStrings.length)]);
-        c.setUserName(names[random.nextInt(names.length)]);
-        c.setUserPicture(userPics[random.nextInt(userPics.length)]);
+
         return c;
     }
 
     public static Feed getRandomFeed() {
         Feed f = new Feed();
-        f.setTitle(phrases[random.nextInt(phrases.length)]);
-        f.setUserPicUrl(userPics[random.nextInt(userPics.length)]);
+        int r = random.nextInt();
+        f.setTitle(getUserName(r) + phrases[random.nextInt(phrases.length)]);
+        f.setUserPicUrl(getUserPicture(r));
         f.setCommentsCount(random.nextInt(30));
         f.setFavoritesCount(random.nextInt(40));
         f.setSharesCount(random.nextInt(15));
@@ -139,10 +147,27 @@ public class MockUtils {
 
     public static User getRandomUser() {
         User user = new User();
-        user.setName(names[random.nextInt(names.length)]);
-        user.setUserPicture(userPics[random.nextInt(userPics.length)]);
+        int r = random.nextInt();
+        user.setName(getUserName(r));
+        user.setUserPicture(getUserPicture(r));
         user.setSocialMedia(getRandomSocialResource());
         return user;
+    }
+
+    public static String getUserPicture(int r) {
+        if(r % 2 == 0) {
+            return maleUserPics[random.nextInt(maleUserPics.length)];
+        } else {
+            return femaleUserPics[random.nextInt(femaleUserPics.length)];
+        }
+    }
+
+    public static String getUserName(int r) {
+        if(r % 2 == 0) {
+            return maleNames[random.nextInt(maleNames.length)];
+        } else {
+            return femaleNames[random.nextInt(femaleNames.length)];
+        }
     }
 
     public static int getRandomSocialResource() {
