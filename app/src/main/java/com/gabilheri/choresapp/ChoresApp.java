83d7f9666c1c4909e4fbe345ed8ddf9527ff5a1d
marcus <marcusandreog@gmail.com>
2015-08-07 00:33:02 -0500

    * Fix to sign in * Exposed application singleton * Exposed API through application singleton

--- app/src/main/java/com/gabilheri/choresapp/ChoresApp.java
+++ app/src/main/java/com/gabilheri/choresapp/ChoresApp.java
@@ -3,6 +3,8 @@ package com.gabilheri.choresapp;
 import android.app.Application;
 
 import com.facebook.FacebookSdk;
+import com.gabilheri.choresapp.data.ChoresApi;
+import com.gabilheri.choresapp.data.NetworkClient;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 
@@ -21,11 +23,28 @@ public class ChoresApp extends Application {
     private static final String TWITTER_KEY = "whvjumpAlIDRZg7HbK0EzbfYp";
     private static final String TWITTER_SECRET = "5LozfFt9ejPT5QDlr7mnNb7og5uJJO01qHc6s6YnCehXciulfc";
 
+    private static ChoresApp sInstance;
+
+    private ChoresApi api;
+
     @Override
     public void onCreate() {
         super.onCreate();
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
         FacebookSdk.sdkInitialize(this);
+        sInstance = this;
     }
+
+    public static ChoresApp instance() {
+        return sInstance;
+    }
+
+    public ChoresApi getApi() {
+        if(api == null) {
+            api = NetworkClient.getApi(this);
+        }
+        return api;
+    }
+
 }
