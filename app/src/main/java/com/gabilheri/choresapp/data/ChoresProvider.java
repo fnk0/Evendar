a89233b8fce98982f965cc03cb6e581426b08267
Kieran Stolorz <kieranstolorz12@gmail.com>
2015-08-10 23:20:29 -0700

    provider/uri matching

--- app/src/main/java/com/gabilheri/choresapp/data/ChoresProvider.java
+++ app/src/main/java/com/gabilheri/choresapp/data/ChoresProvider.java
@@ -6,10 +6,12 @@ import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 
 import static com.gabilheri.choresapp.data.ChoresContract.EventEntry;
+import static com.gabilheri.choresapp.data.ChoresContract.PATH_USER;
 import static com.gabilheri.choresapp.data.ChoresContract.UserEntry;
 import static com.gabilheri.choresapp.data.ChoresContract.CommentEntry;
 import static com.gabilheri.choresapp.data.ChoresContract.RSVPEntry;
@@ -28,25 +30,37 @@ public class ChoresProvider extends ContentProvider {
     private static final UriMatcher sUriMatcher = buildUriMatcher();
     private ChoresDbHelper mChoresDbHelper;
 
+    private static final SQLiteQueryBuilder sChores
+
     // User matcher ID's
     private static final int USER = 100;
     private static final int USER_WITH_ID = 101;
     private static final int USER_WITH_USERNAME = 102;
+    private static final int USER_WITH_EVENT_ID = 103;
 
     // Event Mather ID's
     private static final int EVENTS = 200;
+    private static final int EVENT_WITH_STARTDATE = 201;
+    private static final int EVENT_WITH_ENDDATE = 202;
+    private static final int EVENT_WITH_IS_WANT = 203;
 
     // Comments Matcher ID's
     private static final int COMMENTS = 300;
+    private static final int COMMENT_WITH_ID = 301;
 
     // Friendship matcher ID's
     private static final int FRIENDS = 400;
+    private static final int FRIEND_WITH_ID = 401;
+    private static final int FRIEND_WITH_USER_ID = 402;
 
     // RSVP Matcher ID's
     private static final int RSVP = 500;
+    private static final int RSVP_WITH_ID = 501;
+    private static final int RSVP_WITH_EVENT_ID = 502;
 
     // Favorites Matcher ID's
     private static final int FAVORITES = 600;
+    private static final int FAVORITE_WITH_ID = 601;
 
     @Override
     public boolean onCreate() {
@@ -55,6 +69,44 @@ public class ChoresProvider extends ContentProvider {
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+       Cursor retCursor;
+        switch(sUriMatcher.match(uri)){
+            case USER_WITH_EVENT_ID:
+            {
+                retCursor =;
+                break;
+            }
+
+            case USER_WITH_USERNAME:
+            {
+                retCursor = ;
+                break;
+            }
+            case USER_WITH_ID:
+            {
+                retCursor = ;
+                break;
+
+            }
+            case USER:
+            {
+                retCursor = null;
+                break;
+            }
+
+            case EVENTS:
+            {
+                retCursor = ;
+                break;
+            }
+            case EVENT_WITH_ENDDATE:
+            {
+                retCursor = ;
+                break;
+
+            }
+        }
+
         return null;
     }
 
@@ -191,21 +243,33 @@ public class ChoresProvider extends ContentProvider {
         matcher.addURI(authority, ChoresContract.PATH_USER, USER);
         matcher.addURI(authority, ChoresContract.PATH_USER + "/id/#", USER_WITH_ID);
         matcher.addURI(authority, ChoresContract.PATH_USER + "/us/*", USER_WITH_USERNAME);
+        matcher.addURI(authority, ChoresContract.PATH_USER + "/ei/#", USER_WITH_EVENT_ID);
 
         // Events URI's
         matcher.addURI(authority, ChoresContract.PATH_EVENT, EVENTS);
+        matcher.addURI(authority, ChoresContract.PATH_EVENT + "/s/*", EVENT_WITH_STARTDATE);
+        matcher.addURI(authority, ChoresContract.PATH_EVENT + "/e/*", EVENT_WITH_ENDDATE);
+        matcher.addURI(authority, ChoresContract.PATH_EVENT + "/iw/#", EVENT_WITH_IS_WANT);
+
 
         // Comments URI's
         matcher.addURI(authority, ChoresContract.PATH_COMMENTS, COMMENTS);
+        matcher.addURI(authority, ChoresContract.PATH_COMMENTS + "/commentId/#", COMMENT_WITH_ID);
 
         // Favorites URI's
         matcher.addURI(authority, ChoresContract.PATH_FAVORITES, FAVORITES);
+        matcher.addURI(authority, ChoresContract.PATH_FAVORITES + "/favId/#", FAVORITE_WITH_ID);
 
         // Friends URI's
         matcher.addURI(authority, ChoresContract.PATH_FRIENDSHIP, FRIENDS);
+        matcher.addURI(authority, ChoresContract.PATH_FRIENDSHIP + "/fId/#", FRIEND_WITH_ID);
+        matcher.addURI(authority, ChoresContract.PATH_FRIENDSHIP + "/userId/#", FRIEND_WITH_USER_ID);
+
 
         // RSVP Uri's
         matcher.addURI(authority, ChoresContract.PATH_RSVP, RSVP);
+        matcher.addURI(authority, ChoresContract.PATH_RSVP + "/rsvpId/#", RSVP_WITH_ID);
+        matcher.addURI(authority, ChoresContract.PATH_RSVP + "/eId/#", RSVP_WITH_EVENT_ID);
 
         return matcher;
     }
